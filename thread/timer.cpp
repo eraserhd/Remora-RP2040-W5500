e0a20f4540b9b2a70fcbecc3e76ec4b40228573a
Jason Felice <jason.m.felice@gmail.com>
2026-03-29 12:20:59 -0400

    Fix subtle drift issue

--- thread/timer.cpp
+++ thread/timer.cpp
@@ -15,14 +15,14 @@
 void PWM_Wrap_Handler0()
 {
     hw_clear_bits(&timer_hw->intr, 1u << 0);
-    timer_hw->alarm[0] = timer_hw->timerawl + BASE_PERIOD;
+    timer_hw->alarm[0] += BASE_PERIOD;
     Interrupt::SLICE0_Wrapper();
 }
 
 void PWM_Wrap_Handler1()
 {
     hw_clear_bits(&timer_hw->intr, 1u << 1);
-    timer_hw->alarm[1] = timer_hw->timerawl + SERVO_PERIOD;
+    timer_hw->alarm[1] += SERVO_PERIOD;
     Interrupt::SLICE1_Wrapper();
 }
 
