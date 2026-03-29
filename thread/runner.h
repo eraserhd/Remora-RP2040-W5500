be392c6ae38ce2d0a531842bf59f83ae23cf450b
Jason Felice <jason.m.felice@gmail.com>
2026-03-29 18:37:39 -0400

    Simplify

--- thread/runner.h
+++ thread/runner.h
@@ -18,34 +18,31 @@ struct NormalRunContext
     static void run(pruThread* thread);
 };
 
-template<int32_t irq, int32_t bit, int32_t freq, typename RunContextType>
-struct ThreadRunner
+template<int32_t Irq, int32_t Bit, int32_t Freq, typename RunContext>
+class ThreadRunner
 {
-    typedef RunContextType RunContext;
-    static constexpr int32_t IRQ = irq;
-    static constexpr int32_t BIT = bit;
-    static constexpr int32_t PERIOD = 1000000 / freq;
-
 private:
+    static constexpr int32_t Period = 1000000 / Freq;
+
     static pruThread *thread;
     static void handleAlarmInterrupt()
     {
-        hw_clear_bits(&timer_hw->intr, 1u << BIT);
-        timer_hw->alarm[BIT] += PERIOD;
+        hw_clear_bits(&timer_hw->intr, 1u << Bit);
+        timer_hw->alarm[Bit] += Period;
         RunContext::run(thread);
     }
 
 public:
     static void start(pruThread *_thread)
     {
-        printf("    setting up timer Slice %d\n", BIT);
-        printf("    actual period = %d\n", PERIOD);
+        printf("    setting up timer Slice %d\n", Bit);
+        printf("    actual period = %d\n", Period);
 
         thread = _thread;
-        hw_set_bits(&timer_hw->inte, 1u << BIT);
-        irq_set_exclusive_handler(IRQ, handleAlarmInterrupt);
-        irq_set_enabled(IRQ, true);
-        timer_hw->alarm[BIT] = timer_hw->timerawl + PERIOD;
+        hw_set_bits(&timer_hw->inte, 1u << Bit);
+        irq_set_exclusive_handler(Irq, handleAlarmInterrupt);
+        irq_set_enabled(Irq, true);
+        timer_hw->alarm[Bit] = timer_hw->timerawl + Period;
 
         printf("    timer started\n");
     }
