d8868c0440518169b3645d040b4a07093b0de6e4
Jason Felice <jason.m.felice@gmail.com>
2026-03-29 12:41:00 -0400

    Consolodate

--- thread/timer.cpp
+++ thread/timer.cpp
@@ -4,13 +4,46 @@
 #include <stdio.h>
 
 #include "../configuration.h"
-#include "timerInterrupt.h"
 #include "timer.h"
 #include "pruThread.h"
 
 
 #define BASE_PERIOD 1000000 / PRU_BASEFREQ
 #define SERVO_PERIOD 1000000 / PRU_SERVOFREQ
+
+// Base class for all interrupt derived classes
+
+#define PERIPH_COUNT_IRQn   8               // Total number of device interrupt sources - 8 PWM Slices (for the moment)
+
+
+class Interrupt
+{
+protected:
+    static Interrupt* ISRVectorTable[PERIPH_COUNT_IRQn];
+
+public:
+    Interrupt(void);
+
+    static void Register(int interruptNumber, Interrupt* intThisPtr);
+
+    // wrapper functions to ISR_Handler()
+    static void SLICE0_Wrapper();
+    static void SLICE1_Wrapper();
+
+    virtual void ISR_Handler(void) = 0;
+};
+
+class TimerInterrupt : public Interrupt
+{
+private:
+    pruTimer* InterruptOwnerPtr;
+
+public:
+    TimerInterrupt(int interruptNumber, pruTimer* ownerptr);
+
+    void ISR_Handler(void);
+};
+
 
 // Define the vector table, it is only declared in the class declaration
 Interrupt* Interrupt::ISRVectorTable[] = {0};
