24c55600fd785151d714ef7371f22f4bd1a387a9
Jason Felice <jason.m.felice@gmail.com>
2026-03-29 12:39:57 -0400

    Consolodate

--- thread/timer.cpp
+++ thread/timer.cpp
@@ -8,8 +8,49 @@
 #include "timer.h"
 #include "pruThread.h"
 
+
 #define BASE_PERIOD 1000000 / PRU_BASEFREQ
 #define SERVO_PERIOD 1000000 / PRU_SERVOFREQ
+
+// Define the vector table, it is only declared in the class declaration
+Interrupt* Interrupt::ISRVectorTable[] = {0};
+
+// Constructor
+Interrupt::Interrupt(void){}
+
+// Methods
+
+void Interrupt::Register(int interruptNumber, Interrupt* intThisPtr)
+{
+    printf("Registering interrupt for interrupt number = %d\n", interruptNumber);
+    ISRVectorTable[interruptNumber] = intThisPtr;
+}
+
+void Interrupt::SLICE0_Wrapper(void)
+{
+    ISRVectorTable[0]->ISR_Handler();
+}
+
+void Interrupt::SLICE1_Wrapper(void)
+{
+    ISRVectorTable[1]->ISR_Handler();
+}
+
+
+TimerInterrupt::TimerInterrupt(int interruptNumber, pruTimer* owner)
+{
+    // Allows interrupt to access owner's data
+    InterruptOwnerPtr = owner;
+
+    // When a device interrupt object is instantiated, the Register function must be called to let the
+    // Interrupt base class know that there is an appropriate ISR function for the given interrupt.
+    Interrupt::Register(interruptNumber, this);
+}
+
+void TimerInterrupt::ISR_Handler(void)
+{
+    this->InterruptOwnerPtr->timerTick();
+}
 
 void PWM_Wrap_Handler0()
 {
