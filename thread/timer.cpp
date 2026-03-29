6dac875ec23ea66a5c71d5f411f5de1d2e2e7337
Jason Felice <jason.m.felice@gmail.com>
2026-03-29 15:26:38 -0400

    Move this

--- thread/timer.cpp
+++ thread/timer.cpp
@@ -7,22 +7,6 @@
 #include "timer.h"
 #include "pruThread.h"
 
-struct BaseThreadTimer
-{
-    static constexpr int32_t IRQ = TIMER_IRQ_0;
-    static constexpr int32_t BIT = 0;
-    static constexpr int32_t PERIOD = 1000000 / PRU_BASEFREQ;
-    static pruThread *thread;
-};
-
-struct ServoThreadTimer
-{
-    static constexpr int32_t IRQ = TIMER_IRQ_1;
-    static constexpr int32_t BIT = 1;
-    static constexpr int32_t PERIOD = 1000000 / PRU_SERVOFREQ;
-    static pruThread *thread;
-};
-
 pruThread *BaseThreadTimer::thread = NULL;
 pruThread *ServoThreadTimer::thread = NULL;
 
--- thread/timer.h
+++ thread/timer.h
@@ -2,8 +2,26 @@
 #define TIMER_H
 
 #include <stdint.h>
+#include "hardware/irq.h"
+#include "../configuration.h"
 
 class pruThread; // forward declaration
+
+struct BaseThreadTimer
+{
+    static constexpr int32_t IRQ = TIMER_IRQ_0;
+    static constexpr int32_t BIT = 0;
+    static constexpr int32_t PERIOD = 1000000 / PRU_BASEFREQ;
+    static pruThread *thread;
+};
+
+struct ServoThreadTimer
+{
+    static constexpr int32_t IRQ = TIMER_IRQ_1;
+    static constexpr int32_t BIT = 1;
+    static constexpr int32_t PERIOD = 1000000 / PRU_SERVOFREQ;
+    static pruThread *thread;
+};
 
 class pruTimer
 {
