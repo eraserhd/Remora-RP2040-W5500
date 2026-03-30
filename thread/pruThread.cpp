2dd52099baade0c16806b0cbe4a7de75dedba506
Jason Felice <jason.m.felice@gmail.com>
2026-03-30 18:10:46 -0400

    Consistently use SLICE defines

--- thread/pruThread.cpp
+++ thread/pruThread.cpp
@@ -15,12 +15,12 @@ pruThread::pruThread(uint8_t slice)
 {
     printf("Creating thread\n");
     
-    if (this->slice == 0){
+    if (this->slice == BASE_SLICE) {
         gpio_init(6);
         gpio_set_dir(6, 1);
     }
 
-    if (this->slice == 1){
+    if (this->slice == SERVO_SLICE) {
         gpio_init(27);
         gpio_set_dir(27, 1);
     }   
@@ -30,9 +30,9 @@ pruThread::pruThread(uint8_t slice)
 
 void pruThread::startThread(void)
 {
-    if (0 == slice)
+    if (BASE_SLICE == slice)
         BaseThreadRunner::start(this);
-    else if (1 == slice)
+    else if (SERVO_SLICE == slice)
         ServoThreadRunner::start(this);
 }
 
@@ -47,22 +47,22 @@ void pruThread::run(void)
     if(!this->execute)
         return; 
     
-    if (this->slice == 0){
+    if (this->slice == BASE_SLICE) {
         gpio_put(6, 1);
     }
 
-    if (this->slice == 1){
+    if (this->slice == SERVO_SLICE) {
         gpio_put(27, 1);
     }
 
     for (auto iter = vThread.begin(); iter != vThread.end(); ++iter) (*iter)->runModule();
     for (auto iter = vThread.begin(); iter != vThread.end(); ++iter) (*iter)->runModulePost();
 
-    if (this->slice == 0){
+    if (this->slice == BASE_SLICE){
         gpio_put(6, 0);
     }
 
-    if (this->slice == 1){
+    if (this->slice == SERVO_SLICE){
         gpio_put(27, 0);
     }
 
