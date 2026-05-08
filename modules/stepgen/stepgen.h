e509689257177451e2490890e6b7e2c0e3219867
Jason Felice <jason.m.felice@gmail.com>
2026-05-08 14:54:04 -0400

    Move definitions

--- modules/stepgen/stepgen.h
+++ modules/stepgen/stepgen.h
@@ -62,6 +62,64 @@ public:
     void stopPulses();
 };
 
+template<class Pin>
+void BasicStepgen<Pin>::update()
+{
+    // Use the standard Module interface to run makePulses()
+    this->makePulses();
+}
+
+template<class Pin>
+void BasicStepgen<Pin>::updatePost()
+{
+    this->stopPulses();
+}
+
+template<class Pin>
+void BasicStepgen<Pin>::slowUpdate()
+{
+    return;
+}
+
+template<class Pin>
+void BasicStepgen<Pin>::makePulses()
+{
+    rxData_t *rxData = getCurrentRxBuffer(&rxPingPongBuffer);
+    bool isEnabled = (rxData->jointEnable & (1 << jointNumber)) != 0;
+    if (!isEnabled)
+        return;
+
+    int32_t frequencyCommand = rxData->jointFreqCmd[this->jointNumber];  // Get the latest frequency command via pointer to the data source
+    int32_t DDSaddValue = frequencyCommand * this->frequencyScale;       // Scale the frequency command to get the DDS add value
+    int32_t stepNow = this->DDSaccumulator;                              // Save the current DDS accumulator value
+    this->DDSaccumulator += DDSaddValue;                                 // Update the DDS accumulator with the new add value
+    stepNow ^= this->DDSaccumulator;                                     // Test for changes in the low half of the DDS accumulator
+    stepNow &= (1L << STEPBIT);                                          // Check for the step bit
+
+    if (!stepNow)
+        return;
+
+    bool isForward = DDSaddValue > 0;
+    this->directionPin->set(isForward);                                  // Set direction pin
+    this->stepPin->set(true);                                            // Raise step pin
+    if (isForward)
+    {
+        ++this->rawCount;
+    }
+    else
+    {
+        --this->rawCount;
+    }
+    txData_t *txData = getCurrentTxBuffer(&txPingPongBuffer);
+    txData->jointFeedback[this->jointNumber] = this->rawCount;
+}
+
+template<class Pin>
+void BasicStepgen<Pin>::stopPulses()
+{
+    this->stepPin->set(false);  // Reset step pin
+}
+
 typedef BasicStepgen<Pin> Stepgen;
 
 #endif
