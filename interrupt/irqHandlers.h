e549dc435940fe7e2e908213d75f732a552e283b
Jason Felice <jason.m.felice@gmail.com>
2026-05-11 20:43:29 -0400

    More consistent timing

--- interrupt/irqHandlers.h
+++ interrupt/irqHandlers.h
@@ -6,13 +6,13 @@ extern "C" {
 	void PWM_Wrap_Handler0()
 	{
 		hw_clear_bits(&timer_hw->intr, 1u << 0);
-		timer_hw->alarm[0] = timer_hw->timerawl + BASE_PERIOD;
-		Interrupt::SLICE0_Wrapper();		
+		timer_hw->alarm[0] += BASE_PERIOD;
+		Interrupt::SLICE0_Wrapper();
 	}	
 	void PWM_Wrap_Handler1()
 	{
 		hw_clear_bits(&timer_hw->intr, 1u << 1);
-		timer_hw->alarm[1] = timer_hw->timerawl + SERVO_PERIOD;
+		timer_hw->alarm[1] += SERVO_PERIOD;
 		Interrupt::SLICE1_Wrapper();
-	}		
+	}
 }
